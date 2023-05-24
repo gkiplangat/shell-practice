51ead8b29202e9680ac8e55c9a8af9d53ca213df
gkiplangat <gkiplangat>
2023-05-24 17:50:34 +0300

    readline function in main.c

--- main.c
+++ main.c
@@ -13,6 +13,38 @@ void prompt(void)
 }
 
 /**
+ * read_line - Gets a typed line from the command line as a string.
+ * Return: Pointer to the line(string).
+ */
+
+char *read_line(void)
+{
+	char *line =  NULL;
+	size_t len = 0;
+	/*int chars = 0;*/
+	ssize_t chars = 0;
+
+	/* inbuilt getline */
+	chars = getline(&line, &len, stdin);
+
+	/* custom get line */
+	/*chars = get_line(&line);*/
+	if (chars == EOF)
+	{
+		free(line);
+		exit(0);
+	}
+	if (line == NULL)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		free(line);
+		exit(-1);
+	}
+
+	return (line);
+}
+
+/**
  * main - Entry to Simple Shell program.
  * @argc: Number of arguments.
  * @argv: Pointer to array of arguments.
